5f3963e5681c02754cb853c11eb2bf7e2652a720
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-02 02:11:47 +0100

    Test

--- src/pages/daniel.ts
+++ src/pages/daniel.ts
@@ -2,7 +2,14 @@ import { APIRoute } from 'astro';
 
 
 
-export const post: APIRoute = ({ request }) => {
+export const post: APIRoute = async ({ request }) => {
+
+
+    const data = await request.json();
+
+    console.log("Data:", data)
+
+
     return {
       body: JSON.stringify({
         message: "This was a POST!"
