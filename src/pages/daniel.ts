9282355b53eceb431d6cc5c078e41be1e41f4ebf
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-02 03:04:02 +0100

    test

--- src/pages/daniel.ts
+++ src/pages/daniel.ts
@@ -1,35 +1,18 @@
 import { APIRoute } from "astro";
 
 export const post: APIRoute = async ({ request }) => {
-  const data = await request.json();
 
-  // console.log("Data:", data)
-
-  return {
-    status: 200,
-
-    headers: {
-      "Content-Type": "application/json",
-    },
-
-    body: JSON.stringify({
-      //message: "This was a POST!",
-      message: data
-    }),
-  };
-};
-
-/* return {
-        status: 200,
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({ email: 'hello world' }),
-      };*/
-
-/* return {
-      body: JSON.stringify({
-        name: 'Astro',
-        url: 'https://astro.build/',
-      }),
-    };*/
+    if (request.headers.get('Content-Type') === 'application/json') {
+     // const formData = await request.json()
+      //const name = formData.name
+      return new Response(
+        JSON.stringify({
+          message: 'Your name was: ',
+        }),
+        {
+          status: 200,
+        }
+      )
+    }
+    return new Response(null, { status: 400 })
+  }
