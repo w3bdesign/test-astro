ee52715a30e1825cb1611ddec4601ff57e120aca
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-02 05:35:59 +0100

    test

--- src/pages/api/daniel.ts
+++ src/pages/api/daniel.ts
@@ -1,59 +1,37 @@
 import { APIRoute } from "astro";
 
+export const post: APIRoute = async ({ params, request }) => {
+  if (request.headers.get("Content-Type") === "application/json") {
+    // const formData = await request.json()
+    //const name = formData.name
+    return new Response(
+      JSON.stringify({
+        message: "Your name was: ",
+      }),
+      {
+        status: 200,
+      }
+    );
+  }
+  //return new Response(null, { status: 400 })
+
+  return new Response(
+    JSON.stringify({
+      message: "Your error: " + request.headers,
+    }),
+    {
+      status: 200,
+    }
+  );
+};
+
+
 
 
 export const get: APIRoute = ({ params, request }) => {
-    return {
-      body: JSON.stringify({
-        message: "This was a GET!"
-      })
-    }
+  return {
+    body: JSON.stringify({
+      message: "This was a GET!",
+    }),
   };
-
-
-
-
-
-
-
-
-
-
-
-
-export const post: APIRoute = async ({ request }) => {
-
-    if (request.headers.get('Content-Type') === 'application/json') {
-     // const formData = await request.json()
-      //const name = formData.name
-      return new Response(
-        JSON.stringify({
-          message: 'Your name was: ',
-        }),
-        {
-          status: 200,
-        }
-      )
-    }
-    //return new Response(null, { status: 400 })
-
-
-   
-
-
-
-    return new Response(
-        JSON.stringify({
-          message: 'Your error: ' + request.headers,
-        }),
-        {
-          status: 200,
-        }
-      )
-
-
-
-
-
-
-  }
+};
