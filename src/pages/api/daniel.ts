92a18c4199ed3a36f0959854b982a3a994472097
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-02 05:42:35 +0100

    Update daniel.ts

--- src/pages/api/daniel.ts
+++ src/pages/api/daniel.ts
@@ -3,6 +3,11 @@ import { APIRoute } from "astro";
 export const post: APIRoute = async ({ params, request }) => {
 
 
+  console.log("Params:", params)
+
+  console.log("Request:", request)
+
+
   return {
     body: JSON.stringify({
       message: "This was a POST!",
