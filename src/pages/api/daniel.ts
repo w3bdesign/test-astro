0595b67e4c4d530ff21f0d80a4e50490a7c1ae51
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-02 06:20:58 +0100

    Update daniel.ts

--- src/pages/api/daniel.ts
+++ src/pages/api/daniel.ts
@@ -1,4 +1,4 @@
-import { APIRoute } from "astro";
+/*import { APIRoute } from "astro";
 
 export const post: APIRoute = async ({ params, request }) => {
 
@@ -25,4 +25,13 @@ export const get: APIRoute = ({ params, request }) => {
       message: "This was a GET!",
     }),
   };
+};*/
+
+export const config = {
+  runtime: 'edge',
 };
+
+export default (req: Request) => {
+  return new Response(`Hello, from ${req.url} I'm now an Edge Function!`);
+};
+
