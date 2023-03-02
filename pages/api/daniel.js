293089fe948d28d6c13cc8521aafe135478eac78
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-02 06:28:38 +0100

    Create daniel.js

---
+++ pages/api/daniel.js
@@ -0,0 +1,39 @@
+import { APIRoute } from "astro";
+
+export const post = async ({ params, request }) => {
+
+
+  console.log("Params:", params)
+
+  console.log("Request:", request)
+
+
+  return {
+    body: JSON.stringify({
+      message: "This was a POST!",
+    }),
+  };
+  
+};
+
+
+
+
+export const get = ({ params, request }) => {
+  return {
+    body: JSON.stringify({
+      message: "This was a GET!",
+    }),
+  };
+};
+
+/*
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default (req: Request) => {
+  return new Response(`Hello, from ${req.url} I'm now an Edge Function!`);
+};*/
+
